e89bafc7ce0e6454aac8e79577cc8aebe89218f4
Aditya <aditya.mishra791@berkeley.edu>
2016-03-20 20:57:41 -0700

    Changed auto.py to accept cmd args

--- auto.py
+++ auto.py
@@ -1,8 +1,19 @@
-import os
+import os, sys
 
-cryptoBaseDir = "C:\\Users\Aditya\\Desktop\\Crypter\\Scripts\\"
-input = "C:\\Users\\Aditya\\cryptoComputer\\data2\\Crypters\\Benign_Exe\\" 
-output = "C:\\Users\Aditya\\Desktop\\Output\\"
+total = len(sys.argv)
+cmdargs = sys.argv
+
+cryptoBaseDir = cmdargs[1]
+input = cmdargs[2]
+output = cmdargs[3]
+
+print(cryptoBaseDir)
+print(input)
+print(output)
+
+#cryptoBaseDir = "C:\\Users\Aditya\\Desktop\\Crypter\\Scripts\\"
+#input = "C:\\Users\\Aditya\\cryptoComputer\\data2\\Crypters\\Benign_Exe\\" 
+#output = "C:\\Users\Aditya\\Desktop\\Output\\"
 
 for file in os.listdir(cryptoBaseDir):
     if file.endswith(".exe"):
